ce3118a0f18547ff481cdf3a3e5da2fa0309634b
Eric Bailey <eric@ericb.me>
2016-02-19 15:03:53 -0600

    Add WordNumber skeleton

---
+++ src/WordNumber.hs
@@ -0,0 +1,28 @@
+{-|
+Module      : WordNumber
+Copyright   : (c) Eric Bailey, 2016
+License     : MIT
+
+Maintainer  : Eric Bailey
+Stability   : experimental
+Portability : portable
+
+Chapter 8: Recursion
+-}
+
+module WordNumber
+       ( digitToWord
+       , digits
+       , wordNumber
+       ) where
+
+import           Data.List (intersperse)
+
+digitToWord :: Int -> String
+digitToWord = undefined
+
+digits :: Int -> [Int]
+digits = undefined
+
+wordNumber :: Int -> String
+wordNumber = undefined
