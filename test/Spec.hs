ad6497d57c9d985dc6d4c3d9e0cd7fd4f6d5b7e9
Eric Bailey <eric@ericb.me>
2016-02-19 14:45:08 -0600

    Add test for Recursion.mc91
    Also, add the necessary HUnit dependency.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import           Data.Bool   (bool)
+import           Recursion   (mc91)
+import           System.Exit (ExitCode (..), exitWith)
+import           Test.HUnit  (Assertion, Counts (..), Test (..), runTestTT,
+                              (@=?))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = exitProperly $ runTestTT tests
+
+tests :: Test
+tests =
+  TestList [ testCase "The McCarthy 91 function: [95..110]" $
+             [91,91,91,91,91,91,91,92,93,94,95,96,97,98,99,100] @=?
+             map mc91 ([95..110] :: [Integer])
+           ]
+
+testCase :: String -> Assertion -> Test
+testCase label assertion = TestLabel label (TestCase assertion)
+
+exitProperly :: IO Counts -> IO ()
+exitProperly m = do
+  counts <- m
+  let nonZero = ((0 /=) . ($ counts))
+      exit    = exitWith . bool ExitSuccess (ExitFailure 1)
+  exit $ any nonZero [failures, errors]
